70384f25b4ce44f557cb3e059624ae8757c265a2
adityapb <adityapb1546@gmail.com>
2015-05-21 15:26:58 +0530

    Add GetVideo

---
+++ Music/GetVideo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from YoutTubeId import YouTube
+import urllib
+import json
+
+class Music:
+	url = ""
+	YOUTUBE_API_KEY = ""
+
+	def __init__(self, api_key, tag):
+		self.url = 'http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=' + tag + '&api_key=e17566c75feedfd740e58e0251cf4eb9&format=json'
+		self.YOUTUBE_API_KEY = api_key
+
+	def GetTracks(self):
+		page = urllib.urlopen(self.url)
+		jObj = json.load(page)
+		tracks = []
+		for dict in jObj["toptracks"]["track"]:
+			tracks.append(dict["name"] + ", " + dict["artist"]["name"])
+		return tracks
+
+	def GetVideoURL(self):
+		BASE_URL = "https://www.youtube.com/watch?v="
+		tracks = self.GetTracks()
+		y = YouTube(self.YOUTUBE_API_KEY)
+		urls = []
+		for track in tracks:
+			urls.append(BASE_URL + y.GetID(track))
+		return urls
