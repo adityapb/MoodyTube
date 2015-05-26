87e2129b54c5ff38a919cce07b7533c76424e89f
Aditya Bhosale <140010009@iitb.ac.in>
2015-05-26 18:15:01 +0530

    Update Image class

--- Mood_Detection/Image.py
+++ Mood_Detection/Image.py
@@ -14,21 +14,21 @@ class Image:
 		#self.IMG_PATH = img
 		
 	def cropImg(self, *args):
-		faceCascade = cv2.CascadeClassifier(self.CASCADE_PATH)
+		Cascade = cv2.CascadeClassifier(self.CASCADE_PATH)
 		#os.chdir(self.IMG_PATH)
 		crop_img = []
 		for filename in args:
 			image = cv2.imread(str(filename))
 			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-			faces = faceCascade.detectMultiScale(
+			objects = Cascade.detectMultiScale(
 			    gray,
 			    scaleFactor=1.1,
 			    minNeighbors=5,
 			    minSize=(30, 30),
 			    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 			)
-			print "Found {0} faces!".format(len(faces))
-			for (x, y, w, h) in faces:
+			print "Found {0} objects!".format(len(objects))
+			for (x, y, w, h) in objects:
 				crop_img.append(image[y:y+h , x:x+w])
 		return crop_img
 			
