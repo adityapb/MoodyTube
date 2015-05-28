fbc5b18148b68b616c7af1b5948cb9f82186987a
Aditya Bhosale <140010009@iitb.ac.in>
2015-05-28 19:01:37 +0530

    Update conerDetection method

--- Mood_Detection/Image.py
+++ Mood_Detection/Image.py
@@ -15,13 +15,13 @@ class Image:
 		#self.IMG_PATH = img
 		
 	def identify(self, *args):
-		'''For simplicity args has filenames'''
+		'''args has images'''
 		Cascade = cv2.CascadeClassifier(self.CASCADE_PATH)
 		#os.chdir(self.IMG_PATH)
 		crop_img = []
 		parameters = {}
-		for filename in args:
-			image = cv2.imread(str(filename))
+		for image in args:
+			#image = cv2.imread(str(filename))
 			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 			objects = Cascade.detectMultiScale(
 			    gray,
@@ -32,40 +32,44 @@ class Image:
 			)
 			print "Found {0} objects!".format(len(objects))
 			for (x, y, w, h) in objects:
-				parameters[filename] = (x, y, w, h)
+				if image not in parameters: parameters[image] = [(x, y, w, h)]
+				else: parameters[image].append((x, y, w, h))
 		return parameters
 		
 	def cropImg(self, *args):
-		'''args has filenames'''
+		'''args has images'''
 		crop_img = []
-		for filename, (x, y, w, h) in self.identify(*args).items:
-			crop_img.append(cv2.imread(str(filename))
+		for image, (x, y, w, h) in self.identify(*args).items:
+			crop_img.append(image[y:y+h , x:x+w])
 		return crop_img
 			
 	def saveImg(self, PATH, *args):
-		'''Here args are images and not filenames'''
 		os.chdir(PATH)
 		for i, image in enumerate(args):
 			print "Saved {0} image".format(i)
 			cv2.imwrite(str(i) + '.bmp' , image)
 		return
 		
-	def cornerDetect(self, img, x, y, w, h):
+	def cornerDetect(self, img):
 		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		gray = np.float32(gray)
 		dst = cv2.cornerHarris(gray,2,3,0.04)
+		#(x, y, w, h) = identify(img)
 		#result is dilated for marking the corners, not important
 		dst = cv2.dilate(dst,None)
 		# Threshold for an optimal value, it may vary depending on the image.
 		#if j,i lies in y:y+h , x:x+w; use those coordinates
-		count = 0.
-		for i in [x:x+w]:
-			for j in [y:y+h]:
-				if dst[i][j] > 0.5*dst.max():
-					X += i
-					Y += j
-					count += 1
-		return [X/count , Y/count]
+		corners= []
+		for (x, y, w, h) in identify(img):
+			count = 0.
+			for i in [x:x+w]:
+				for j in [y:y+h]:
+					if dst[i][j] > 0.5*dst.max():
+						X += i
+						Y += j
+						count += 1
+			corners.append([X/count , Y/count])
+		return corners
 		
 	
 	def rotateImage(self, image, angle):
@@ -76,7 +80,8 @@ class Image:
 	
 	
 	def alignEyes(self, *args):
-		pass
+		'''args has images'''
+		
 	
 	def alignImg(self, *args):
 		'''Align eyes and mouth in all images
@@ -96,7 +101,7 @@ if __name__ == '__main__':
 		BASE_IMG_PATH = str(sys.argv[2])
 		images = []
 		for filename in glob.glob(BASE_IMG_PATH + '/*.bmp'):
-			images.append(filename)
+			images.append(cv2.imread(filename))
 		cropped = img.cropImg(*images)
 		img.saveImg(str(os.getcwd()) + '/cropped', *cropped)
 	
