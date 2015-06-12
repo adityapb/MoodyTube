f4bf4d2c085cfdb744d1bdf85040dc4297770eef
Aditya Bhosale <adityapb1546@gmail.com>
2015-06-12 15:12:45 +0530

    Update PCA clas

--- Mood_Detection/eigenfaces.py
+++ Mood_Detection/eigenfaces.py
@@ -10,15 +10,20 @@ import heapq
 
 class PCA:
 	
-	def __init__(self, *args):
+	def __init__(self, length, *args):
+		self.length = length
 		self.images = args
 		
 	def eigenfaces(self, K):
 		faces = []
+		res = []
 		eig = self.eigenvectors()
 		eigenvals = heapq.nlargest(K, eig)
+		print eigenvals
 		for key in eigenvals:
 			faces.append(eig[key])
+		for i, face in enumerate(faces):
+			faces[i] = np.array(self.reconstruct(face.tolist()))
 		return faces
 			
 	def rowVec(self, image):
@@ -28,6 +33,13 @@ class PCA:
 			#print row
 			result = np.concatenate([result, row])
 		return result
+		
+	def reconstruct(self, vec, length = None):
+		if length is None: length = self.length
+		res = [[] for x in range(length)]
+		for i, val in enumerate(vec):
+			res[i/length].append(val)
+		return res
 		
 	def imageMat(self):
 		mat = []
@@ -40,7 +52,7 @@ class PCA:
 		
 	def eigenvectors(self):
 		res = {}
-		eigenvals, eigenvecs = np.linalg.eig(self.covMat())
+		eigenvals, eigenvecs = np.linalg.eigh(self.covMat())
 		for i, val in enumerate(eigenvals):
 			res[val] = eigenvecs[i]
 		return res
@@ -51,6 +63,6 @@ if __name__ == '__main__':
 		for filename in glob.glob(os.getcwd() + '/male/*.bmp'):
 			print filename
 			filenames.append(cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2GRAY))
-		p = PCA(*filenames)
+		p = PCA(50, *filenames)
 		print p.eigenfaces(3)
 	
