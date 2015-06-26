9b3ece30a60bc7de8d670d7a1b15b4c40033c808
Aditya Bhosale <adityapb1546@gmail.com>
2015-06-26 23:49:29 +0530

    Update preprocessing methods

--- Mood_Detection/PreProcessing.py
+++ Mood_Detection/PreProcessing.py
@@ -3,18 +3,17 @@
 import cv2
 import numpy as np
 
-class PreProcessing:
+def gamma_correction(correction, image):
+	img = image/255.0
+	img = cv2.pow(img, correction)
+	return np.uint8(img*255)
 	
-	def __init__(self):
-		pass
+def histogram_equalize(image):
+	return cv2.equalizeHist(image)
+	
+def gray(image):
+	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	
+def dog_filter(img):
+	return cv2.GaussianBlur(img,img.shape,0)
 		
-	def gamma_correction(self, correction, image):
-		img = image/255.0
-		img = cv2.pow(img, correction)
-		return np.uint8(img*255)
-		
-	def histogram_equalize(self, image):
-		return cv2.equalizeHist(image)
-		
-	def gray(self, image):
-		return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
